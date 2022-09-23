72250068fe549890eb59956c5160a59c2993d47e
guanxiaodan <xiaodan.guan@okg.com>
2022-09-23 18:47:25 +0800

    类的继承和修饰器

---
+++ src/types/class.ts
@@ -0,0 +1,48 @@
+class Dog {
+  constructor(name: string) {
+    this.name = name;
+  }
+  name: string;
+  run() {}
+}
+
+// 无论是ES还是TS，类成员的属性都是实例属性，而不是原型属性，类成员的方法都是原型方法
+// 类的私有成员（private）只能被类本身调用，不能被类的实例调用，也不能被子类调用
+// 如果给类的构造函数前面加private，就说明这个类既不能被实例化，也不能被继承
+
+console.log(Dog.prototype); // 打印类的原型，只有类成员的方法run,没有类成员的属性name
+let dog = new Dog("wang");
+console.log("gxd", "dog:", dog); // 类成员的属性name在这个dog实例上,而不在原型上
+
+// TS中实例的属性必须有初始值，或者在构造函数中被初始化
+class Dogs {
+  constructor(name: string) {}
+  name: string = "wang";
+  run() {}
+}
+// 类的继承
+
+// 定义子类
+class Husky extends Dog {
+  constructor(name: string, color: string) {
+    super(name); // super代表父类的实例，父类构造函数中的参数，子类也要有
+    this.color = color;
+  }
+  color: string;
+}
+
+// 类的成员修饰符（ts对es6的扩展）
+// public 公有成员(默认类的属性和方法都是公有属性，公有属性可以被实例化或继承)
+// private 私有成员  类的私有成员（private）只能被类本身调用，不能被类的实例调用，也不能被子类调用，如果给类的构造函数前面加private，就说明这个类既不能被实例化，也不能被继承
+// protected 受保护成员 （只能在类或者子类中使用。不能在类的实例中访问），如果在构造函数前面加protected，说明这个类只能被继承，不能被实例化
+// readonly 只读成员  只读属性和实例属性一样，必须被初始化
+// static 静态成员 类的静态成员只能通过类名来调用， 不能通过实例来调用，可以被子类继承，即子类也可以调用静态成员
+
+// 除了类的成员可以添加修饰符，构造函数的参数也可以添加修饰符，作用是将参数自动变为实例的属性，就不用再类中声明该属性了，使代码更简洁。比如：
+class Husky2 extends Dog {
+  constructor(name: string, public color: string) {
+    super(name); // super代表父类的实例，父类构造函数中的参数，子类也要有
+    this.color = color;
+  }
+  // color: string; // 因为在构造函数中添加了修饰符public，所以color就变成了实例的属性，就不用在Husky2中声明该属性了
+}
